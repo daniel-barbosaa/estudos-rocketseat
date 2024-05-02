1f592384a0e42b397b25c921d7e69dd99a0866a0
DaniMendess <danimendes9728@gmail.com>
2024-05-02 14:53:31 -0300

    listando posts na tela já formatados quando feito a busca da API

--- projetos/ignews/src/pages/posts/index.tsx
+++ projetos/ignews/src/pages/posts/index.tsx
@@ -3,11 +3,22 @@ import style from './style.module.scss'
 
 import { createClient } from '../../services/prismicio'
 import { GetStaticProps } from 'next'
+import { RichText } from 'prismic-dom'
 
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string
+}
 
-export default  function Posts() {
+interface PostsProps {
+    posts: Post[]
+}
 
-    
+export default  function Posts({posts}: PostsProps) {
+
+    console.log(posts)
     
     return(
         <>
@@ -16,31 +27,15 @@ export default  function Posts() {
          </Head>
          <main className={style.container}>
             <div className={style.post_list}>
-                <a href="#">
-                    <time>
-                    12 de março de 2021
-                    </time>
-                    <strong>Creating a Monorepo with Lerna & Yarn Workspaces1</strong>
-                    <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-                </a>
- 
-
-                <a href="#">
-                    <time>
-                    12 de março de 2021
-                    </time>
-                    <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                    <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-                </a>
-
-            
-                <a  href="#">
-                    <time>
-                    12 de março de 2021
-                    </time>
-                    <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                    <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-                </a>
+               {posts.map(post => (
+                 <a href="#" key={post.slug}>
+                 <time>
+                     {post.updatedAt}
+                 </time>
+                 <strong>{post.title}</strong>
+                 <p>{post.excerpt}</p>
+             </a>
+               ))}
                 </div>
          </main>
         </>
@@ -54,9 +49,27 @@ export const getStaticProps: GetStaticProps = async ()  => {
     const response = await prismic.getAllByType('publication')
     console.log(JSON.stringify(response, null, 2))
 
+    const posts = response.map(post => {
+
+        const excerptNode = post.data.content.find(content => content.type === 'paragraph');
+        const excerpt = excerptNode && 'text' in excerptNode ? excerptNode.text : '';
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: excerpt,
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+
+    
+
     return {
         props: {
-
+            posts
         }
     }
 }
