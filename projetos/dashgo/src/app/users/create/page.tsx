02a573c563bb275e4d65880b55adb2d57262c655
DaniMendess <danimendes9728@gmail.com>
2024-06-03 20:03:53 -0300

    criando página de criação de usuário, sub rota aninhada]

---
+++ projetos/dashgo/src/app/users/create/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import Input from "@/components/Form/Input";
+import { Header } from "@/components/Header";
+import { Sidebar } from "@/components/Sidebar";
+import { Box, Button, Divider, Flex, HStack, Heading, Icon, SimpleGrid, VStack } from "@chakra-ui/react";
+import { RiSaveLine } from "react-icons/ri";
+
+export default function CreateUser() {
+    return (
+        <Box>
+            <Header/>
+            <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+                <Sidebar/>
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8" >
+                    <Heading size="lg" fontSize="normal">Criar usuário</Heading>
+                    <Divider my="6" borderColor="gray.700"/>
+                    <VStack spacing="8">
+                        <SimpleGrid minChildWidth="240px" w="100%" spacing="8">
+                            <Input name="nome" label="Nome Completo"/>
+                            <Input name="email" type="email" label="E-mail"/>
+                        </SimpleGrid>
+                        <SimpleGrid minChildWidth="240px" w="100%" spacing="8">
+                            <Input name="password" type="password" label="Senha"/>
+                            <Input name="confirmation_password" type="password" label="Confirmação de senha"/>
+                        </SimpleGrid>
+                    </VStack>
+                    
+                    <Flex mt="8" justify="flex-end">
+                        <HStack spacing="4">
+                            <Button colorScheme="whiteAlpha">Cancelar</Button>
+                            <Button colorScheme="pink" leftIcon={<Icon as={RiSaveLine}/>}>Salvar</Button>
+                        </HStack>
+                    </Flex>
+                    
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
