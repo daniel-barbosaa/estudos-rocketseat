5c57949b3f8b920ca9a311f7f84d724d6004db81
DaniMendess <danimendes9728@gmail.com>
2024-06-04 20:22:38 -0300

    responsividade do header feita

--- projetos/dashgo/src/components/Header/index.tsx
+++ projetos/dashgo/src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 'use client'
-import { Flex} from "@chakra-ui/react"
+import { Flex, useBreakpointValue} from "@chakra-ui/react"
 import { Profile } from "./Profile"
 import { NotificatioNav } from "./NotificationNav"
 import { SearchBox } from "./SearchBox"
 import { Logo } from "./Logo"
 
 export function Header () {
+    const isWideVersion = useBreakpointValue({
+      base: false,
+      lg: true
+    })
     return (
     <Flex
       as="header"
@@ -17,12 +21,12 @@ export function Header () {
       px="6"
       align="center">
       <Logo/>
-      <SearchBox/>
+      {isWideVersion && <SearchBox/>}
       <Flex
         align="center"
         ml="auto">
             <NotificatioNav/>
-            <Profile/>
+            <Profile showProfileData={isWideVersion}/>
        </Flex>
     </Flex>
     )
