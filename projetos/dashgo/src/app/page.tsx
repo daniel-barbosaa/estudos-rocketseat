fe4e352ee067c4f0befc065b72841575304ec265
DaniMendess <danimendes9728@gmail.com>
2024-06-08 12:57:42 -0300

    validando formulários da aplicação com react hook form

--- projetos/dashgo/src/app/page.tsx
+++ projetos/dashgo/src/app/page.tsx
@@ -1,16 +1,29 @@
 'use client'
 import {Button, Flex, Stack} from '@chakra-ui/react'
+import { useForm, SubmitHandler } from "react-hook-form"
+import { Input } from '@/components/Form/Input'
 
-import Input from '@/components/Form/Input'
+interface SignInFormData {
+    email: string,
+    password: string
+}
 
+// Corrigir erro da linha 23 
 
 export default function SignIn () {
+    const {register, handleSubmit} = useForm()
+
+    const handleSignIn: SubmitHandler<SignInFormData> = (values) => {
+        console.log(values)
+    }
+
+
     return (
        <Flex w="100vw" h="100vh" align="center" justify="center">
-        <Flex as="form" w="100%" maxW={360} p={8} bg="gray.800" borderRadius={8} flexDirection="column">
+        <Flex as="form" w="100%" maxW={360} p={8} bg="gray.800" borderRadius={8} flexDirection="column" onSubmit={handleSubmit(handleSignIn)}>
             <Stack spacing={4}>
-               <Input type="email" name="email" label="E-mail"/>
-               <Input  type="password" name="password" label='Senha'/>
+               <Input type="email" label="E-mail" {...register('email')}/>
+               <Input  type="password" label='Senha' {...register('password')}/>
             </Stack>
             <Button colorScheme='pink' type='submit' mt={6} size="lg">Entrar</Button>
         </Flex>
--- projetos/dashgo/src/components/Form/Input.tsx
+++ projetos/dashgo/src/components/Form/Input.tsx
@@ -1,20 +1,29 @@
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react"
+import { ForwardRefRenderFunction, forwardRef } from "react"
 
 interface  InputProps extends ChakraInputProps {
     name: string,
     label?: string
 }
-
-export function Input ({ name, label, ...rest }: InputProps) {
+const InputBase:ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
     return (
         <FormControl>
             { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-            <ChakraInput id={name} name={name }type='email' focusBorderColor='pink.500' bg="gray.900" variant="filled" 
-             _hover={{bgColor: 'gray.900'}} size="lg"
-             {...rest}
-            />
+            <ChakraInput
+              id={name}
+              name={name
+              }type='email'
+              focusBorderColor='pink.500'
+              bg="gray.900"
+              variant="filled"
+              _hover={{bgColor:
+              'gray.900'}}
+              size="lg"
+              {...rest}
+              ref={ref} 
+              />
         </FormControl>
     )
 }
 
-export default Input
+export const Input = forwardRef(InputBase)
